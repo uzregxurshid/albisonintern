89653bb9d263bcc5249c7b04be105954b829afdf
uzregxurshid <uzregxurshid@gmail.com>
2022-06-13 10:24:07 +0500

    fixed

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 const styles = {
-  contact: 'contact bg-transparent',
+  contact: 'contact bg-transparent -mt-20',
   container: 'container max-w-[1316px] px-5 mx-auto',
-  contact__container: 'contact__container bg-mainbggreen flex flex-col items-center  pt-[78px] pb-[95px]',
-  contact__header: 'contact__header text-[44px] leading-[53px] font-bold text-maintext text-center not-italic ',
-  contact__mail: 'pl-[19px] bg-transparent border-[1px] rounded border-maintext w-[400px] h-16 text-maintext leading-[53px] text-[20px] font-bold mt-[57px] placeholder:text-maintext placeholder:leading-[53px] placeholder:text-[20px] placeholder:font-bold placeholder:mt-[57px]',
+  contact__container: 'contact__container bg-mainbggreen flex flex-col items-center  pt-[78px] pb-[95px] md:px-2 sm:py-15',
+  contact__header: 'contact__header text-[44px] leading-[53px] font-bold text-maintext text-center not-italic md:text-[38px]',
+  contact__mail: 'sm:w-[285px] pl-[19px] bg-transparent border-[1px] rounded border-maintext w-[400px] h-16 text-maintext leading-[53px] text-[20px] font-bold mt-[57px] placeholder:text-maintext placeholder:leading-[53px] placeholder:text-[20px] placeholder:font-bold placeholder:mt-[57px]',
   contact__button: 'ml-4 w-[116px] h-16 rounded bg-maintext text-mainbggreen text-center leading-[53px] text-[20px] font-bold mt-[57px]',
 
 }
@@ -15,7 +15,7 @@ const Contacts = () => {
         <div className={styles.contact__container}>
           <h2 className={styles.contact__header}>You have the power to define your future.</h2>
           <form action="/">
-            <input className={styles.contact__mail} type="email" name="email" id="email" placeholder='EMAIL ADDRESS' required="true" />
+            <input className={styles.contact__mail} type="email" name="email" id="email" placeholder='EMAIL ADDRESS' required="required" />
             <button className={styles.contact__button} type='submit'> SUBMIT</button>
           </form>
         </div>
